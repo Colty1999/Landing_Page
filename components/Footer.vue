edf7bcb992fbc8672698af89d527876b67522e95
Colty1999 <mateuszgietka99@gmail.com>
2024-02-01 04:46:12 +0100

    error page created

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="fixed z-100 bottom-0 w-full px-[10%] h-12 text-primary-text tracking-wider">
       <div class="flex justify-start">
         <div class="flex gap-1 items-center">
-          <a href="#"
+          <a href="https://github.com/Colty1999" target="_blank" rel="noopener noreferrer"
             class="relative w-8 h-8 inline-flex items-center justify-center bg-transparent border-0.1 border-tertiary rounded-full text-tertiary no-underline overflow-hidden group">
             <span class="z-10 relative transition duration-300 ease-in-out group-hover:text-secondary-text">
               <font-awesome-icon icon="fa-brands fa-github" />
@@ -10,7 +10,7 @@
             <span
               class="absolute top-0 left-0 w-full h-full bg-tertiary z-0 transform -translate-x-full transition duration-300 ease-in-out group-hover:translate-x-0"></span>
           </a>
-          <a href="#"
+          <a href="https://www.linkedin.com/in/mgietka/" target="_blank" rel="noopener noreferrer"
             class="relative w-8 h-8 inline-flex items-center justify-center bg-transparent border-0.1 border-tertiary rounded-full text-tertiary no-underline overflow-hidden group">
             <span class="z-10 relative transition duration-300 ease-in-out group-hover:text-secondary-text">
               <font-awesome-icon icon="fa-brands fa-linkedin-in" />
---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mt-7 max-w-sm mx-auto text-start">
+        <p class="flex gap-6 items-end">
+            <span class="mt-7 text-6xl font-bold">{{ error.statusCode }}</span>
+            <span class="mt-7 text-5xl">Ooops.</span>
+        </p>
+        <p class="mt-1">{{ error.message }}</p>
+        <button @click="handleGoBack" class="my-1 relative inline-block group">Go Home
+            <span
+                class="absolute bottom-0 left-0 w-full h-0.5 bg-primary-text transform scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["error"]);
+
+const handleGoBack = () => {
+    clearError({ redirect: "/" });
+};
+</script>
